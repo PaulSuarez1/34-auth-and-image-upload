b5251d1186606ce87a485a8d2ae429e5667725d9
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-27 10:20:22 -0700

    finished with this for now because I need to focus on whitebaord prep I'll come back to this tonight and get the rest done

--- app/src/main/java/com/example/paulsuarez/a34_auth_and_image_upload/FeedActivity.java
+++ app/src/main/java/com/example/paulsuarez/a34_auth_and_image_upload/FeedActivity.java
@@ -5,15 +5,27 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeedActivity extends AppCompatActivity {
 
     @BindView(R.id.feed) public RecyclerView recyclerView;
     public LinearLayoutManager linearLayoutManager;
+    public PostAdapter postAdapter;
+
+    private List<Post> mPost;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        ButterKnife.bind(this);
+
+        mPosts = new ArrayList<>();
+
+
     }
 }
--- app/src/main/java/com/example/paulsuarez/a34_auth_and_image_upload/PostAdapter.java
+++ app/src/main/java/com/example/paulsuarez/a34_auth_and_image_upload/PostAdapter.java
@@ -3,14 +3,11 @@ package com.example.paulsuarez.a34_auth_and_image_upload;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "LOGIN";
-
-    private FirebaseAuth mAuth;
+public class PostAdapter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login); // might need to be activity_main
+        setContentView(R.layout.activity_post_adapter);
     }
 }
