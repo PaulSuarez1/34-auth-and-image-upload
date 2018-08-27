6c6de5b45c9db3d5d185f0d01a45cfd1e6fa7c78
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-27 08:14:25 -0700

    getting the project set up

---
+++ app/src/main/java/com/example/paulsuarez/a34_auth_and_image_upload/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.paulsuarez.a34_auth_and_image_upload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
